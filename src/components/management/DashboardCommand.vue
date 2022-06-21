ab7d9b9195ae1d7954c60103b79b9db496c6587b
Ophélien Fouques <ophelien.fouques@viacesi.fr>
2022-06-21 16:32:51 +0200

    gestion client + dashboard

---
+++ src/components/management/DashboardCommand.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="dashboardCommand">
+        <div class="item-a tile">test</div>
+        <div class="item-b tile">test</div>
+        <div class="item-c tile">test</div>
+        <tile-management class="item-d"/>
+    </div>
+
+</template>
+
+<script>
+    import TileManagement from "./TileManagement";
+
+    export default {
+        name: "DashboardCommand",
+        components: {TileManagement},
+    }
+</script>
+
+<style scoped lang="scss">
+    .dashboardCommand {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        grid-template-rows: [row1-start] 50px [row1-end] 100px [third-line] auto [last-line];
+        grid-template-areas: "header header header . main ." "sidebar sidebar sidebar other other other";
+        column-gap: 10px;
+        row-gap: 15px;
+
+        .tile {
+            border-radius: 10px;
+            box-shadow: 5px 5px 10px #d9d9d9,
+            -5px -5px 10px #ffffff;
+        }
+    }
+
+    .item-a {
+        grid-area: header;
+    }
+
+    .item-b {
+        grid-area: main;
+    }
+
+    .item-c {
+        grid-area: sidebar;
+    }
+
+    .item-d {
+        grid-area: other;
+    }
+</style>
---
+++ src/components/management/TileManagement.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="tileManagement">
+        <p class="">3</p>
+        <h3>Nombre de chez pas quoi</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TileManagement"
+    }
+</script>
+
+<style scoped lang="scss">
+    .tileManagement{
+        border-radius: 10px;
+        box-shadow:  5px 5px 10px #d9d9d9,
+        -5px -5px 10px #ffffff;
+    }
+</style>
