861f245c06f7a453606ffbe577719ac50d3f9639
Léo Gilles <leo.gilles123@gmail.com>
2022-06-29 16:06:03 +0200

    test

--- src/views/client/CommandesView.vue
+++ src/views/client/CommandesView.vue
@@ -9,7 +9,9 @@
 
 <script>
     import CommandeComponent from "../../components/CommandeComponent";
-    import axios from 'axios'
+     import store from '../../store'
+    import axios from 'axios';
+    import moment from 'moment';
     export default {
         name: "CommandesView",
         components: {CommandeComponent},
@@ -17,34 +19,95 @@
         data(){
             return{
                 historyCmd: [],
-                restoName: ''
+                restoName: '',
+                role: 0,
+                userId:0,
             }
         },
 
-        methods:{
-            getHistoriqueCmd(){
-                axios.get('http://localhost:1000/gateway/api/commande/now/'+123)
-                    .then(response => {
-                        console.log(response.data)
-                        let verifCmd= []
-                        for (let i =0; i<response.data.length; i++){
-                            if (response.data[i].Status<7||response.data[i].Status===8){
-                                console.log(response.data[i])
-                                verifCmd.push(response.data[i])
-                            }
-                        }
-                        this.historyCmd = verifCmd
-                        //this.restoName(response.data.RestaurantId)
-                    })
-                    .catch(error => {
-                        console.log(error)
-                    })
+        methods: {
+            refreshCommand() {
+                     if (this.role == 1) {
+                         let config = {
+                        method: 'get',
+                        url: 'http://localhost:4000/api/commande/client/' + store.state.userId,
+                        headers: {}
+                    };
+                    console.log("client")
+                    axios(config)
+                        .then((response) => {
+                            this.historyCmd = response.data
+                            this.historyCmd.forEach(cmd => {
+                                cmd.dateTimeCommander = moment(cmd.dateTimeCommander).format('MMMM Do YYYY, h:mm:ss a')
+                                    let config3 = {
+                                        method: 'get',
+                                        url: 'http://localhost:3000/api/Restaurant/' + cmd.RestaurantId,
+                                        headers: {}
+                                    };
+                                    axios(config3)
+                                        .then((response3) => {
+                                            cmd.RestaurantId = response3.data['Nom']
+                                            
+                                        })
+                                        .catch((error) => {
+                                            console.log(error);
+                                        });
+                            })
+                            
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });       
+                     }
+                     else if (this.role == 3){
+let config3 = {
+                        method: 'get',
+                        url: 'http://localhost:3000/api/RestaurantObj/' + store.state.userId,
+                        headers: {}
+                    };
+                    axios(config3)
+                        .then((response3) => {
+                            let config = {
+                                method: 'get',
+                                url: 'http://localhost:4000/api/commande/restaurant/' + response3.data["_id"],
+                                headers: {}
+                            };
+
+
+
+                            axios(config)
+                                .then((response) => {
+                                    this.historyCmd = response.data
+                                })
+                                .catch((error) => {
+                                    console.log(error);
+                                });
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });
+                     }
             }
-        },
-
-
+            },
         mounted() {
-            this.getHistoriqueCmd()
+            let config = {
+                method: 'get',
+                url: 'http://localhost:10432/api/Users/' + store.state.userId,
+                headers: {}
+            };
+
+            axios(config)
+                .then((response) => {
+                   
+                    store.commit('SetUserRole', response.data[0]["Roles"])
+                    this.role = store.state.userRole
+                    this.userId = store.state.userId
+                    
+                    this.refreshCommand()
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         }
     }
 </script>
